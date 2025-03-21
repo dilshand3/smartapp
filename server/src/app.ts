751c160825c017a3920fe03ccfce4b1c2a4b5433
dilshan.d3 <dilshan72404@gmail.com>
2025-03-21 21:28:52 +0530

    production error

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true }))
 app.get("/", (req: Request, res: Response): void => {
-    res.send("my smart app is running properly")
+    res.send("my smart app is running properly now this will work fine")
 });
 
 import TodoRoute from './routes/Todo.routes';
